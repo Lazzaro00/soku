bf9e69a2787da0e2c91aeeea4850c30eb13cf258
STDN2 <132349056+STDN2@users.noreply.github.com>
2023-05-04 20:58:03 +0200

    Update Song.js

--- src/componenti/Song.js
+++ src/componenti/Song.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import foto from '../assets/img/apple.jpg';
+import foto from '../assets/img/DimmiPerché.jpg';
 
 class Song extends React.Component {
     render() {
@@ -11,10 +11,10 @@ class Song extends React.Component {
             <h1>Dimmi Perché - Soku</h1>
         <p className='Pulsanti'>
             <p>
-            <button className='ButtonSong' onClick={() => window.open('https://open.spotify.com/track/0PrJaXzNau6S3AP7prD5Ni?si=cdf627e2d86f4861','_blank')}>ASCOLTA | Spotify</button>
+            <button className='ButtonSong' onClick={() => window.open('https://open.spotify.com/album/38jCfZDkZM0RnTZPbsbZH5','_blank')}>ASCOLTA | Spotify</button>
             <button className='ButtonSong' onClick={() => window.open('https://music.amazon.it/albums/B0BWVHLS3W?marketplaceId=APJ6JRA9NG5V4&musicTerritory=IT&ref=dm_sh_yjobBsAuVTYLiLgXvxkb0J0Tb&trackAsin=B0BWVG7K7C','_blank')}>ASCOLTA | Amazoon Music</button>
             <button className='ButtonSong' onClick={() => window.open('https://music.apple.com/us/album/apple/1674360540?i=1674360541','_blank')}>ASCOLTA | Apple Music</button>
-            <button className='ButtonSong' onClick={() => window.open('https://youtu.be/eRXEDxCf2pM','_blank')}>ASCOLTA | Youtube</button>
+            <button className='ButtonSong' onClick={() => window.open('https://https://youtu.be/3O0NhRiNOCc','_blank')}>ASCOLTA | Youtube</button>
             </p>
         </p>
       </header>
