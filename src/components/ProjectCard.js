25e530410156c088794c2fc3a3764423e5ea636d
Lazzaro00 <lazzaro.carlucci@libero.it>
2023-05-06 19:28:26 +0200

    Update ProjectCard.js

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,10 +2,15 @@ import { Col } from "react-bootstrap";
 import { Outlet, Link } from "react-router-dom";
 
 export const ProjectCard = ({ title, description, imgUrl, link}) => {
+  
+  const handleClick = () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+  
   return (
     <div className='prog' style={{ maxWidth: "600px" }}>
     <Col size={50} sm={6} md={4} >
-    <Link to={link}>
+    <Link to={link} onClick={handleClick}>
       <div className="proj-imgbx" >
         <img src={imgUrl}/>
         <div className="proj-txtx">
